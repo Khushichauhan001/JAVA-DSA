b4feaacaa3b057e3c7d3b92e8adc87f2034f5760
Khushi <khushichauhan2506@gmail.com>
2025-05-09 23:48:17 +0530

    added

---
+++ String/SumOfBeauty.java
@@ -0,0 +1,34 @@
+package String;
+
+public class SumOfBeauty {
+    public static int beautySum(String s) {
+        int totalBeauty = 0;
+        int n = s.length();
+
+        for (int i = 0; i < n; i++) {
+            int[] freq = new int[26];
+
+            for (int j = i; j < n; j++) {
+                freq[s.charAt(j) - 'a']++;
+
+                int maxFreq = 0;
+                int minFreq = Integer.MAX_VALUE;
+
+                for (int k = 0; k < 26; k++) {
+                    if (freq[k] > 0) {
+                        maxFreq = Math.max(maxFreq, freq[k]);
+                        minFreq = Math.min(minFreq, freq[k]);
+                    }
+                }
+
+                totalBeauty += (maxFreq - minFreq);
+            }
+        }
+
+        return totalBeauty;
+    }
+    public static void main(String[] args) {
+        String s= " Khushi";
+        System.out.println(beautySum(s));
+    }
+}
