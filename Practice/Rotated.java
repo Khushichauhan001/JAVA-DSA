637255b40d005180468a5eda554f6f0499a8a1ea
Khushi <khushichauhan2506@gmail.com>
2025-07-18 22:29:16 +0530

    added

--- Practice/Rotated.java
+++ Practice/Rotated.java
@@ -1,23 +1,34 @@
 package Practice;
 
-public class Rotated {
-    public static boolean isRotated(int[] arr){
-         int n = arr.length;
-         int count = 0 ;
-         for(int i=0 ; i<n;i++){
-             if(arr[i] > arr[(i+1)%n]){
+import java.util.Scanner;
 
-                 count++;
-             }
-         }
-         if(count > 1){
-            return false ;
-        }
-        return true ;
-    }
+public class Rotated {
+    // public static boolean isRotated(int[] arr){
+    //      int n = arr.length;
+    //      int count = 0 ;
+    //      for(int i=0 ; i<n;i++){
+    //          if(arr[i] > arr[(i+1)%n]){
+
+    //              count++;
+    //          }
+    //      }
+    //      if(count > 1){
+    //         return false ;
+    //     }
+    //     return true ;
+    // }
+
+    // public static void main(String[] args) {
+    //     // int[] arr ={ 3,4,5,65,1,2};
+    //     // System.out.println(isRotated(arr));
+    //     Scanner sc = new Scanner(System.in);
+        
+    // }
 
     public static void main(String[] args) {
-        int[] arr ={ 3,4,5,65,1,2};
-        System.out.println(isRotated(arr));
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println("Hey" + x);
+        sc.close();
     }
 }
