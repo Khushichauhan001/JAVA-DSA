da620320c37657969c6f5c573360b9062b1dd3f5
Khushi <khushichauhan2506@gmail.com>
2025-08-20 22:32:10 +0530

    correction done

---
+++ DP/UniquePath.java
@@ -0,0 +1,42 @@
+package DP;
+
+public class UniquePath {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        int count =0 ;
+        for(int i=0 ;i<m;i++){
+            for(int j=0 ;j< n;j++){
+                if(i==0 || j==0){
+                    dp[i][j] = 1 ;
+                }
+                else {
+                    dp[i][j] = dp[i][j-1] + dp[i-1][j];
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
+
+
+
+// optimized one 
+// class Solution {
+//     public int uniquePaths(int m, int n) {
+//         int[] dp = new int[n];
+        
+//         // First row: only one way to reach each cell
+//         for (int j = 0; j < n; j++) {
+//             dp[j] = 1;
+//         }
+        
+//         // Process remaining rows
+//         for (int i = 1; i < m; i++) {
+//             for (int j = 1; j < n; j++) {
+//                 dp[j] = dp[j] + dp[j-1];
+//             }
+//         }
+        
+//         return dp[n-1];
+//     }
+// }
